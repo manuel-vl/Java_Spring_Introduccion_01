6860f6e479bd9413f9e336bce64addca3081bfbc
manuel-vl <manuel.valencia@mercadolibre.com.co>
2024-02-07 10:49:45 -0500

    Initial commit

---
+++ src/main/java/com/numerosromanos/romanosapi/RomanosApiApplication.java
@@ -0,0 +1,13 @@
+package com.numerosromanos.romanosapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RomanosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RomanosApiApplication.class, args);
+	}
+
+}
